e79729e4af52da7c74576526e9c0e9e110f9f313
islamnashaat03 <islamnashaat03@gmail.com>
2021-09-21 12:06:10 +0200

    finish header and navbar

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,79 @@
-var gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    prefix = require('gulp-autoprefixer'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    beeper = require('beeper'),
-    notifier = require('node-notifier'),
-    cleanCSS = require('gulp-clean-css'),
-    minify = require('gulp-minify');
+var gulp = require("gulp"),
+    concat = require("gulp-concat"),
+    prefix = require("gulp-autoprefixer"),
+    sass = require("gulp-sass")(require("sass")),
+    sourcemaps = require("gulp-sourcemaps"),
+    notifier = require("node-notifier"),
+    cleancss = require("gulp-clean-css");
+    // minify = require("gulp-minify");
 
-gulp.task('SassCompile', function () {
-
-    return gulp.src(['Content/SASS/main-rtl.scss', 'Content/SASS/main-ltr.scss'])
+gulp.task("sassCompile", function () {
+    return gulp
+        .src(["content/sass/main-rtl.scss", "content/sass/main-ltr.scss"])
         .pipe(sourcemaps.init())
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', function (err) {
-            console.log(`-----------------------------------------------------------------`);
-            console.log(err.message);
-            console.log(`-----------------------------------------------------------------`);
-            beeper();
+        .pipe(
+            sass({
+                outputStyle: "compressed"
+            }).on("error", function (err) {
+                console.log(
+                    `-----------------------------------------------------------------`
+                );
+                console.log(err.message);
+                console.log(
+                    `-----------------------------------------------------------------`
+                );
 
-            notifier.notify(
-                {
-                    title: 'Sass Error Compiling',
-                    message: `Error in File : ${err.relativePath} \nError in Line : ${err.line} , Column : ${err.column} `,
-                    sound: false,
-                    wait: false,
-                    timeout: 1
-                },
-                function (err, response) {
-                    // Response is response from notification
-                }
-            );
-            this.emit('end');
-        }))
-        .pipe(prefix('last 2 versions'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('Content/CSS'));
+                notifier.notify({
+                        title: "sass Error Compiling",
+                        message: `Error in File : ${err.relativePath} \nError in Line : ${err.line} , Column : ${err.column} `,
+                        sound: false,
+                        wait: false,
+                        timeout: 1,
+                    },
+                    function (err, response) {
+                        // Response is response from notification
+                    }
+                );
+                this.emit("end");
+            })
+        )
+        .pipe(prefix("last 2 versions"))
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("content/css"));
 });
-gulp.task('concatMinifyAllRtlCss', function () {
-
-    return gulp.src(['../Content/CSS/Vendor/rtl/bootstrapRTL.min.css', '../Content/CSS/Vendor/*.css', '../Content/CSS/main-rtl.css'])
+gulp.task("concatMinifyAllRtlcss", function () {
+    return gulp
+        .src([
+            "../content/css/vendor/rtl/bootstrapRTL.min.css",
+            "../content/css/vendor/*.css",
+            "../content/css/main-rtl.css",
+        ])
         .pipe(sourcemaps.init())
-        .pipe(concat('rtlMinStyle.min.css'))
-        .pipe(cleanCSS())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('../Content/CSS/minifiedStyles'));
+        .pipe(concat("rtlMinStyle.min.css"))
+        .pipe(cleancss())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("../content/css/minifiedStyles"));
 });
-gulp.task('concatMinifyAllLtrCss', function () {
-
-    return gulp.src(['../Content/CSS/Vendor/ltr/bootstrapLTR.min.css', '../Content/CSS/Vendor/*.css', '../Content/CSS/main-ltr.css'])
+gulp.task("concatMinifyAllLtrcss", function () {
+    return gulp
+        .src([
+            "../content/css/vendor/ltr/bootstrapLTR.min.css",
+            "../content/css/vendor/*.css",
+            "../content/css/main-ltr.css",
+        ])
         .pipe(sourcemaps.init())
-        .pipe(concat('ltrMinStyle.min.css'))
-        .pipe(cleanCSS())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('../Content/CSS/minifiedStyles'));
+        .pipe(concat("ltrMinStyle.min.css"))
+        .pipe(cleancss())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("../content/css/minifiedStyles"));
 });
 
+gulp.task("watch", function () {
+    gulp.watch(
+        ["content/sass/*/*.scss", "content/sass/*.scss"],
+        gulp.series(["sassCompile"])
+    );
+    // gulp.watch('content/sass/*/*.scss', gulp.series(['sassCompile','concatMinifyAllRtlcss','concatMinifyAllLtrcss']));
 
-gulp.task('watch', function () {
-    gulp.watch(['Content/SASS/*/*.scss', 'Content/SASS/*.scss'], gulp.series(['SassCompile']));
-    // gulp.watch('Content/SASS/*/*.scss', gulp.series(['SassCompile','concatMinifyAllRtlCss','concatMinifyAllLtrCss']));
-
-     //gulp.watch('Content/Scripts/Pages/*.js', gulp.series('jsCompress'));
+    //gulp.watch('content/Scripts/Pages/*.js', gulp.series('jsCompress'));
 });
