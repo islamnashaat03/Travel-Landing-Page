d1b68fd1582472a0919c3dc2a810cc50687d30d6
islamnashaat03 <islamnashaat03@gmail.com>
2021-09-30 00:45:45 +0200

    add testmonials section (in progress)

--- content/js/main.js
+++ content/js/main.js
@@ -17,4 +17,48 @@ window.onload = function () {
     }
   }
   // END SCROLL TO TOP BUTTON
+
+
+  // START SWIPER 
+
+  // var swiper = new Swiper(".mySwiper", {
+  //   direction: "vertical",
+  //   slidesPerView: 2,
+  //   loop:true,
+  //   spaceBetween: 30,
+  //       navigation: {
+  //         nextEl: ".swiper-button-next",
+  //         prevEl: ".swiper-button-prev",
+  //       },
+  //       breakpoints: {
+  //         // when window width is >= 320px
+  //         320: {
+  //           slidesPerView: 1,
+  //           spaceBetween: 20
+  //         },
+  //         // when window width is >= 480px
+  //         480: {
+  //           slidesPerView: 2,
+  //           spaceBetween: 30
+  //         },
+  //         // when window width is >= 640px
+  //         640: {
+  //           slidesPerView: 3,
+  //           spaceBetween: 40
+  //         }
+  //       },
+  // });
+
+  var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    effect : 'cards',
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 }
